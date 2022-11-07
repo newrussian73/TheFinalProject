269403b3a4a038c2789e0fc9d3e336e475a46114
Шарунов Владислав <Newrussian73@mail.ru>
2022-11-07 13:32:15 +0400

    Смерижвание тестов для main_page and login_page in test_main_page.py и добавление описание в readme

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,8 @@ def test_quest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
