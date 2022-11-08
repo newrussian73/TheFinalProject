ae82dd5889986edbb2348fac9c3bce65a8e8f72d
Шарунов Владислав <Newrussian73@mail.ru>
2022-11-08 13:07:05 +0400

    Исправлен тест для product_page, добавлено описание методов для product_page и base_page

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,27 +1,32 @@
-from base_page import BasePage
-from locators import ProductPageLocators
+from .base_page import BasePage
+from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+#Запускается для основных тестов
     def should_be_add_to_basket(self):
+        self.should_be_add_to_basket_button()
+        self.solve_quiz_and_get_code()
         self.message_of_add_to_basket()
         self.include_name_of_book_in_message_of_add()
         self.message_of_cost_basket()
         self.include_cost_of_book_in_message_of_cost_basket()
 
+#запускается для инициализации кнопки "Добавить в корзину"
     def should_be_add_to_basket_button(self):
-        self.browser.find_element(ProductPageLocators.ADD_TO_BASKET).click()
-
+        button_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        button_basket.click()
+#Проверка присутствия сообщение о добавлении в корзину
     def message_of_add_to_basket(self):
-        assert self.is_element_present(ProductPageLocators.MESSAGE_ADD_BASKET), 'not exist message for add basket'
-
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_ADD_BASKET), 'not exist message for add basket'
+#Проверка названия вхождения названия книги в сообщение о доавблении в корзину
     def include_name_of_book_in_message_of_add(self):
-        assert self.browser.find_element(ProductPageLocators.NAME_OF_GOOD).text \
-               in self.browser.find_element(ProductPageLocators.MESSAGE_ADD_BASKET).text, 'not include name of book'
-
+        assert self.browser.find_element(*ProductPageLocators.NAME_OF_GOOD).text \
+               in self.browser.find_element(*ProductPageLocators.MESSAGE_ADD_BASKET).text, 'not include name of book'
+#Проверка сообщение о стоимости корзины
     def message_of_cost_basket(self):
-        assert self.is_element_present(ProductPageLocators.MESSAGE_COST_BASKET), 'not exist message of cost basket'
-
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_COST_BASKET), 'not exist message of cost basket'
+#Проверка названия вхождения стоимости книги в сообщение о стоимости корзины
     def include_cost_of_book_in_message_of_cost_basket(self):
-        assert self.browser.find_element(ProductPageLocators.COST_OF_BOOK).text \
-               in self.browser.find_element(ProductPageLocators.MESSAGE_COST_BASKET).text, 'not include cost of book'
+        assert self.browser.find_element(*ProductPageLocators.COST_OF_BOOK).text \
+               in self.browser.find_element(*ProductPageLocators.MESSAGE_COST_BASKET).text, 'not include cost of book'
